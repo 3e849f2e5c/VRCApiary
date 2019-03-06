8b72b5c8b93333385f159bf49cfde1dcf7118c9f
3e849f2e5c <3e849f2e5c@pm.me>
2019-03-06 11:54:08 +0200

    navigation menu base

--- js/client/general.js
+++ js/client/general.js
@@ -1,3 +1,5 @@
+let isMenuUp = false;
+
 const sendNotification = (title, body, image, callback) => {
     const options = {
         body: body
@@ -12,10 +14,14 @@ const sendNotification = (title, body, image, callback) => {
 // TODO proper icons and copyright
 const getIconFor = (name) => {
     switch (name) {
-        case "info": return "https://i.imgur.com/YFPOhDF.png";
-        case "debug": return "https://i.imgur.com/vdhVjqM.png";
-        case "ok": return "https://i.imgur.com/mX6fF8K.png";
-        default: return;
+        case "info":
+            return "https://i.imgur.com/YFPOhDF.png";
+        case "debug":
+            return "https://i.imgur.com/vdhVjqM.png";
+        case "ok":
+            return "https://i.imgur.com/mX6fF8K.png";
+        default:
+            return;
     }
 };
 
@@ -29,11 +35,72 @@ const getId = (id) => {
 };
 
 setTimeout(() => {
-    document.getElementById("content").style.opacity = "1";
+    getId("content").style.opacity = "1";
 }, 250);
-document.getElementById("loadingIcon").style.opacity = "0";
+getId("loadingIcon").style.opacity = "0";
 
 const pageLoad = () => {
-    document.getElementById("content").style.opacity = "0";
-    document.getElementById("loadingIcon").style.opacity = "1";
+    getId("content").style.opacity = "0";
+    getId("content").style.opacity = "0";
+    getId("loadingIcon").style.opacity = "1";
 };
+
+const navToPage = (page) => {
+    const menu = getId("navMenu");
+    const content = getId("content");
+    if (menu !== null) {
+        menu.style.opacity = "0";
+        setTimeout(() => {
+            menu.style.visibility = "hidden";
+            if (content !== null) {
+                content.style.filter = "none";
+            }
+        }, 300);
+    }
+    isMenuUp = false;
+    switch (page) {
+        case "home": {
+            pageLoad();
+            setTimeout(() => {
+                document.location = "./home.html";
+            }, 300);
+            break;
+        }
+    }
+};
+
+getId("navHome").addEventListener('click', () => {
+    navToPage("home");
+});
+
+document.addEventListener('keyup', (e) => {
+    const menu = getId("navMenu");
+    const content = getId("content");
+    if (menu === undefined) {
+        return;
+    }
+    switch (e.key) {
+        case " ": {
+            if (isMenuUp === false) {
+                menu.style.visibility = "visible";
+                setTimeout(() => {
+                    menu.style.opacity = "1";
+                    if (content !== null) {
+                        content.style.filter = "blur(4px)";
+                    }
+                });
+                isMenuUp = true;
+            } else {
+                menu.style.opacity = "0";
+                setTimeout(() => {
+                    menu.style.visibility = "hidden";
+                    if (content !== null) {
+                        content.style.filter = "none";
+                    }
+                }, 100);
+                isMenuUp = false;
+            }
+            break;
+        }
+    }
+});
