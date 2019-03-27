be646502ebd0f279a26e3e06edead61b2346625c
3e849f2e5c <3e849f2e5c@pm.me>
2019-03-27 13:21:53 +0200

    Avatar options beginning

--- js/client/avatars.js
+++ js/client/avatars.js
@@ -3,27 +3,79 @@ const renderFavorites = (data) => {
     const doc = document.getElementById("favoriteAvatars");
     for (let i = 0; i < data.length; i++) {
         const avtr = data[i];
-        doc.appendChild(createEntry(avtr.id, avtr.name, avtr.thumbnailImageUrl, avtr.releaseStatus, [
-            createButton("Change", "button-green", () => {
-
-            }),
-            createButton("Remove", "button-red", () => {
-
-            })
-        ]));
+        doc.appendChild(createFavoriteEntry(avtr));
     }
+};
+
+
+// TODO remove timeout
+const createFavoriteEntry = (avatar) => {
+    return createEntry(avatar.id, avatar.name, avatar.thumbnailImageUrl, avatar.releaseStatus, [
+        createButton("Equip", "button-green", () => {
+
+        }),
+        createButton("View author", "button-green", () => {
+
+        }),
+        createButton("Keepsake", "button-green", () => {
+
+        }),
+        createButton("Remove", "button-red", (e) => {
+            // There's gotta be a better way to do this but honestly I just don't care
+            const options = e.srcElement.parentElement;
+            const popup = e.srcElement.parentElement.parentElement.lastElementChild;
+            const card = e.srcElement.parentElement.parentElement;
+            popup.innerHTML = '';
+            popup.style.visibility = "visible";
+            popup.appendChild(createElement("a", "header", "Removing..."));
+            const removeFunc = () => {
+                options.style.visibility = "visible";
+                options.innerHTML = '';
+                options.appendChild(createElement("a", "header", "Removed"));
+                options.appendChild(createButton("Undo", "button-green", () => {
+                    options.parentElement.insertAdjacentElement('afterbegin', createFavoriteEntry(avatar));
+                    options.parentNode.removeChild(card);
+                }));
+                popup.style.visibility = "hidden";
+            };
+            setTimeout(removeFunc, 5000);
+            popup.appendChild(createButton("Cancel", "button-red", () => {
+                popup.style.visibility = "hidden";
+                clearTimeout(removeFunc);
+            }));
+
+        }),
+        createButton("Cancel", "button-red", () => {
+
+        })
+    ])
 };
 
 const renderAvatars = (data) => {
     const doc = document.getElementById("privateAvatars");
     for (let i = 0; i < data.length; i++) {
         const avtr = data[i];
-        doc.appendChild(createEntry(avtr.id, avtr.name, avtr.thumbnailImageUrl, avtr.releaseStatus));
+        doc.appendChild(createEntry(avtr.id, avtr.name, avtr.thumbnailImageUrl, avtr.releaseStatus, [
+            createButton("Equip", "button-green", () => {
+
+            }),
+            createButton("Edit", "button-green", () => {
+
+            }),
+            createButton("Remove", "button-red", () => {
+
+            }),
+            createButton("Cancel", "button-red", () => {
+
+            })
+        ]));
     }
 };
 
 const createEntry = (id, name, image, status, extra) => {
     const entryContainer = createElement("div", "box card-entry-container");
+    const entryOptions = createElement("div", "card-options");
+    const entryDeleteTimeout = createElement("div", "card-options");
     const avatarContainer = createElement("div", "card-inner-container");
     const avatarName = createElement("a", "card-name", name);
     const avatarImage = createElement("img", "card-image");
@@ -38,18 +90,23 @@ const createEntry = (id, name, image, status, extra) => {
             break;
         }
     }
-
     avatarContainer.setAttribute("title", name);
     avatarContainer.appendChild(avatarName);
     avatarContainer.appendChild(avatarImage);
     entryContainer.appendChild(avatarContainer);
     if (extra !== undefined && extra.length !== 0) {
         for (let i = 0; i < extra.length; i++) {
-            if (typeof extra[i] !== "string") {
-                entryContainer.appendChild(extra[i]);
-            }
+            extra[i].addEventListener('click', () => {
+                entryOptions.style.visibility = "hidden";
+            });
+            entryOptions.appendChild(extra[i]);
         }
     }
+    entryContainer.appendChild(createButton("Options", "button-green", () => {
+        entryOptions.style.visibility = "visible";
+    }));
+    entryContainer.appendChild(entryOptions);
+    entryContainer.appendChild(entryDeleteTimeout);
     return entryContainer;
 };
 
