1303854d17d5c8cc6f6ea6e2ceeeb2656e921fcd
3e849f2e5c <3e849f2e5c@pm.me>
2019-04-09 22:07:43 +0300

    worlds part 2

--- js/client/worlds.js
+++ js/client/worlds.js
@@ -1,3 +1,5 @@
+const fakeJson = "[{\"id\":\"wrld_fae3fa95-bc18-46f0-af57-f0c97c0ca90a\",\"name\":\"The room of the rain\",\"authorName\":\"Spanish HUB\",\"totalLikes\":0,\"totalVisits\":0,\"capacity\":27,\"imageUrl\":\"https://api.vrchat.cloud/api/1/file/file_e4041df4-ecab-4494-86d6-7debe18ab8b3/3/file\",\"thumbnailImageUrl\":\"https://api.vrchat.cloud/api/1/file/file_e4041df4-ecab-4494-86d6-7debe18ab8b3/3/file\",\"isSecure\":false,\"releaseStatus\":\"public\",\"organization\":\"vrchat\",\"tags\":[\"author_tag_rain\",\"author_tag_optimized\",\"author_tag_videoplayer\",\"author_tag_chill\",\"author_tag_sleep\",\"system_approved\"],\"favorites\":5640,\"created_at\":\"2019-03-12T02:43:02.384Z\",\"updated_at\":\"2019-04-08T13:25:03.289Z\",\"publicationDate\":\"2019-03-25T18:56:44.343Z\",\"labsPublicationDate\":\"2019-03-12T19:47:27.477Z\",\"visits\":551044,\"popularity\":7,\"heat\":5,\"occupants\":401},{\"id\":\"wrld_6caf5200-70e1-46c2-b043-e3c4abe69e0f\",\"name\":\"The Great Pug\",\"authorName\":\"owlboy\",\"totalLikes\":0,\"totalVisits\":0,\"capacity\":22,\"imageUrl\":\"https://api.vrchat.cloud/api/1/file/file_9585fd74-1f09-42f0-8e7b-6279225f856b/1/file\",\"thumbnailImageUrl\":\"https://d348imysud55la.cloudfront.net/thumbnails/2756521966.thumbnail-500.png\",\"isSecure\":false,\"releaseStatus\":\"public\",\"organization\":\"vrchat\",\"tags\":[\"author_tag_bar\",\"author_tag_hangout\",\"author_tag_social\",\"author_tag_classic\",\"author_tag_stage\",\"system_approved\",\"admin_hide_new\",\"admin_approved\"],\"favorites\":31515,\"created_at\":\"2017-01-19T01:14:54.000Z\",\"updated_at\":\"2019-04-01T07:06:17.583Z\",\"publicationDate\":\"2018-12-07T02:29:17.174Z\",\"labsPublicationDate\":\"none\",\"visits\":8662615,\"popularity\":9,\"heat\":7,\"occupants\":372},{\"id\":\"wrld_496b11e8-25a0-4f35-976d-faae5e00d60e\",\"name\":\"The Box\",\"authorName\":\"DrPotato\",\"totalLikes\":0,\"totalVisits\":0,\"capacity\":25,\"imageUrl\":\"https://api.vrchat.cloud/api/1/file/file_bb914d4b-91d1-47e0-a93a-3328393ee4c6/5/file\",\"thumbnailImageUrl\":\"https://api.vrchat.cloud/api/1/file/file_bb914d4b-91d1-47e0-a93a-3328393ee4c6/5/file\",\"isSecure\":false,\"releaseStatus\":\"public\",\"organization\":\"vrchat\",\"tags\":[\"author_tag_box\",\"author_tag_optimized\",\"system_approved\",\"admin_approved\"],\"favorites\":11738,\"created_at\":\"2018-08-21T20:14:51.979Z\",\"updated_at\":\"2019-03-16T00:53:20.057Z\",\"publicationDate\":\"2018-12-03T21:38:40.943Z\",\"labsPublicationDate\":\"none\",\"visits\":6983896,\"popularity\":8,\"heat\":7,\"occupants\":267},{\"id\":\"wrld_a0ad5ad3-2b2c-4a77-8220-d372d299b412\",\"name\":\"VRChat Home\",\"authorName\":\"vrchat\",\"totalLikes\":0,\"totalVisits\":0,\"capacity\":16,\"imageUrl\":\"https://api.vrchat.cloud/api/1/file/file_1d899df7-a548-41e6-844c-437c8f2eb0aa/2/file\",\"thumbnailImageUrl\":\"https://api.vrchat.cloud/api/1/file/file_1d899df7-a548-41e6-844c-437c8f2eb0aa/2/file\",\"isSecure\":false,\"releaseStatus\":\"public\",\"organization\":\"vrchat\",\"tags\":[\"admin_approved\",\"admin_hide_new\",\"admin_hide_popular\",\"author_tag_home\",\"admin_featured\",\"system_approved\"],\"favorites\":9797,\"created_at\":\"2018-04-04T23:47:06.645Z\",\"updated_at\":\"2018-12-28T21:58:15.406Z\",\"publicationDate\":\"2018-11-10T01:02:45.925Z\",\"labsPublicationDate\":\"none\",\"visits\":29425886,\"popularity\":9,\"heat\":7,\"occupants\":230},{\"id\":\"wrld_c215101b-8f13-408c-8bc6-1687cf87762f\",\"name\":\"Hwabon Night （화본역）\",\"authorName\":\"Bepsi Train\",\"totalLikes\":0,\"totalVisits\":0,\"capacity\":20,\"imageUrl\":\"https://api.vrchat.cloud/api/1/file/file_0ac79bdf-5c7f-4ef5-aadf-3d056b201918/3/file\",\"thumbnailImageUrl\":\"https://api.vrchat.cloud/api/1/file/file_0ac79bdf-5c7f-4ef5-aadf-3d056b201918/3/file\",\"isSecure\":false,\"releaseStatus\":\"public\",\"organization\":\"vrchat\",\"tags\":[\"author_tag_korea\",\"author_tag_snowing\",\"author_tag_train\",\"author_tag_station\",\"author_tag_snow\",\"system_approved\",\"admin_approved\",\"admin_hide_new\",\"admin_community_spotlight\"],\"favorites\":3382,\"created_at\":\"2019-02-07T15:18:36.987Z\",\"updated_at\":\"2019-03-25T22:14:44.680Z\",\"publicationDate\":\"none\",\"labsPublicationDate\":\"none\",\"visits\":849348,\"popularity\":7,\"heat\":6,\"occupants\":145},{\"id\":\"wrld_ccbf8103-c23e-472f-8efb-38a9a9164357\",\"name\":\"Murder‼ 〈GAME〉\",\"authorName\":\"Jar\",\"totalLikes\":0,\"totalVisits\":0,\"capacity\":12,\"imageUrl\":\"https://api.vrchat.cloud/api/1/file/file_1ea914f9-2a82-4bc6-96fa-0268c9d64f80/3/file\",\"thumbnailImageUrl\":\"https://api.vrchat.cloud/api/1/file/file_1ea914f9-2a82-4bc6-96fa-0268c9d64f80/3/file\",\"isSecure\":false,\"releaseStatus\":\"public\",\"organization\":\"vrchat\",\"tags\":[\"admin_approved\",\"admin_hide_new\",\"admin_game\",\"admin_community_spotlight\",\"system_approved\"],\"favorites\":19469,\"created_at\":\"2018-10-05T04:30:41.744Z\",\"updated_at\":\"2019-03-27T21:19:31.168Z\",\"publicationDate\":\"2018-11-10T01:02:45.818Z\",\"labsPublicationDate\":\"none\",\"visits\":2313751,\"popularity\":8,\"heat\":6,\"occupants\":135},{\"id\":\"wrld_6e77e43e-3fdd-447f-9c49-f13351febc0a\",\"name\":\"Never Have I Ever - Game World\",\"authorName\":\"Frisky℻\",\"totalLikes\":0,\"totalVisits\":0,\"capacity\":25,\"imageUrl\":\"https://api.vrchat.cloud/api/1/file/file_4a975b46-6e04-455d-bc1d-9e886eed20d8/6/file\",\"thumbnailImageUrl\":\"https://d348imysud55la.cloudfront.net/thumbnails/3159373421.thumbnail-500.png\",\"isSecure\":false,\"releaseStatus\":\"public\",\"organization\":\"vrchat\",\"tags\":[\"admin_approved\",\"system_approved\"],\"favorites\":7627,\"created_at\":\"2018-02-08T00:57:12.898Z\",\"updated_at\":\"2019-02-23T02:15:40.796Z\",\"publicationDate\":\"2018-12-03T23:37:57.034Z\",\"labsPublicationDate\":\"none\",\"visits\":1000374,\"popularity\":8,\"heat\":6,\"occupants\":135},{\"id\":\"wrld_94ef6736-f998-4099-b456-b3a444734013\",\"name\":\"Open Mic Night\",\"authorName\":\"gunter\",\"totalLikes\":0,\"totalVisits\":0,\"capacity\":36,\"imageUrl\":\"https://api.vrchat.cloud/api/1/file/file_87f30074-4a2f-4421-bc84-ed58804551ca/1/file\",\"thumbnailImageUrl\":\"https://files.vrchat.cloud/thumbnails/4237452660.thumbnail-500.png\",\"isSecure\":false,\"releaseStatus\":\"public\",\"organization\":\"vrchat\",\"tags\":[\"admin_approved\",\"admin_hide_new\",\"system_approved\"],\"favorites\":14799,\"created_at\":\"2017-11-11T02:37:14.115Z\",\"updated_at\":\"2018-11-10T01:02:46.698Z\",\"publicationDate\":\"2018-11-10T01:02:46.698Z\",\"labsPublicationDate\":\"none\",\"visits\":3001863,\"popularity\":8,\"heat\":6,\"occupants\":125},{\"id\":\"wrld_191cdb3d-a6ff-4677-9e83-7831a27807d6\",\"name\":\"Oknuj‘s Hangout n Sleep\",\"authorName\":\"oknuj\",\"totalLikes\":0,\"totalVisits\":0,\"capacity\":32,\"imageUrl\":\"https://api.vrchat.cloud/api/1/file/file_292bd782-5e2d-4516-8782-52765d894dc8/8/file\",\"thumbnailImageUrl\":\"https://api.vrchat.cloud/api/1/file/file_292bd782-5e2d-4516-8782-52765d894dc8/8/file\",\"isSecure\":false,\"releaseStatus\":\"public\",\"organization\":\"vrchat\",\"tags\":[\"author_tag_sleep\",\"author_tag_oknuj\",\"author_tag_comfy\",\"author_tag_hangout\",\"author_tag_snooze\",\"system_approved\",\"admin_approved\"],\"favorites\":14002,\"created_at\":\"2018-06-03T21:43:38.666Z\",\"updated_at\":\"2019-04-01T18:57:19.762Z\",\"publicationDate\":\"2018-11-23T22:14:44.763Z\",\"labsPublicationDate\":\"none\",\"visits\":1800203,\"popularity\":8,\"heat\":6,\"occupants\":120},{\"id\":\"wrld_fff5d510-fc53-4e88-9d4e-1e0e45a17aff\",\"name\":\"Bedroom Theatre\",\"authorName\":\"Temporal\",\"totalLikes\":0,\"totalVisits\":0,\"capacity\":16,\"imageUrl\":\"https://api.vrchat.cloud/api/1/file/file_7022aa4d-9d6f-4c3a-aa6f-9d4f93c0a404/3/file\",\"thumbnailImageUrl\":\"https://api.vrchat.cloud/api/1/file/file_7022aa4d-9d6f-4c3a-aa6f-9d4f93c0a404/3/file\",\"isSecure\":false,\"releaseStatus\":\"public\",\"organization\":\"vrchat\",\"tags\":[\"author_tag_video\",\"author_tag_stream\",\"author_tag_bedroom\",\"author_tag_theater\",\"author_tag_sleep\",\"system_approved\",\"admin_approved\"],\"favorites\":7584,\"created_at\":\"2018-10-01T13:16:41.868Z\",\"updated_at\":\"2019-03-30T04:56:15.031Z\",\"publicationDate\":\"2018-12-05T20:40:58.370Z\",\"labsPublicationDate\":\"none\",\"visits\":649757,\"popularity\":7,\"heat\":6,\"occupants\":94},{\"id\":\"wrld_78373831-0109-4808-9b63-27382f4c6975\",\"name\":\"Presentation Room\",\"authorName\":\"vrchat\",\"totalLikes\":0,\"totalVisits\":0,\"capacity\":32,\"imageUrl\":\"https://api.vrchat.cloud/api/1/file/file_ff75b9f0-fe45-41b1-9d38-dbc6e1b61469/1/file\",\"thumbnailImageUrl\":\"https://api.vrchat.cloud/api/1/file/file_ff75b9f0-fe45-41b1-9d38-dbc6e1b61469/1/file\",\"isSecure\":false,\"releaseStatus\":\"public\",\"organization\":\"vrchat\",\"tags\":[\"content_featured\",\"admin_approved\",\"admin_hide_new\",\"admin_hide_popular\",\"author_tag_creative\",\"author_tag_draw\",\"author_tag_meeting\",\"author_tag_pens\",\"author_tag_art\",\"system_approved\"],\"favorites\":19267,\"created_at\":\"2017-07-31T21:07:51.000Z\",\"updated_at\":\"2019-01-25T23:10:56.923Z\",\"publicationDate\":\"2018-11-10T01:02:45.866Z\",\"labsPublicationDate\":\"none\",\"visits\":3809222,\"popularity\":9,\"heat\":6,\"occupants\":88}]";
+
 // search
 const searchWorlds = getId("searchWorlds");
 // spotlight
@@ -5,9 +7,12 @@ const spotlightWorlds = getId("spotlightWorlds");
 const spotlightLoad = getId("spotlightLoad");
 // active
 let activePage = 0;
+let prevActiveWorlds = [];
 const activeWorlds = getId("activeWorlds");
 const activeLoad = getId("activeLoad");
 // popular
+let popularPage = 0;
+let prevPopularWorlds = [];
 const popularWorlds = getId("popularWorlds");
 const popularLoad = getId("popularLoad");
 // avatar
@@ -32,7 +37,7 @@ const labsLoad = getId("labsLoad");
 const favoriteWorlds = getId("favoriteWorlds");
 const favoriteLoad = getId("favoriteLoad");
 
-const createEntry = (id, name, image, status, extra) => {
+const createEntry = (world, id, name, image, status, extra) => {
     const entryContainer = createElement("div", "box card-entry-container");
     const entryOptions = createElement("div", "card-options");
     const entryDeleteTimeout = createElement("div", "card-options card-remove-timeout");
@@ -50,6 +55,9 @@ const createEntry = (id, name, image, status, extra) => {
             break;
         }
     }
+    avatarContainer.addEventListener('click', () => {
+        entryOptions.style.visibility = "visible";
+    });
     avatarContainer.setAttribute("title", name);
     avatarContainer.appendChild(avatarName);
     avatarContainer.appendChild(avatarImage);
@@ -62,42 +70,56 @@ const createEntry = (id, name, image, status, extra) => {
             entryOptions.appendChild(extra[i]);
         }
     }
-    entryContainer.appendChild(createButton("Options", "button-green", () => {
-        entryOptions.style.visibility = "visible";
-    }));
+    const cardDescription = createElement("div", "card-description-container");
+    const heatBar = createElement("div", "box heat-bar");
+    const heatPercentage = Math.floor((world.heat * 100) / 7);
+    heatBar.style.background = "linear-gradient(90deg, #CB772F " + heatPercentage + "%, rgba(0,0,0,0) " + heatPercentage + "%, rgba(0,0,0,0) 100%)";
+    heatBar.title = "Heat";
+    cardDescription.appendChild(heatBar);
+    cardDescription.appendChild(createElement("a", "", "Users: " + world.occupants));
+    entryContainer.appendChild(cardDescription);
     entryContainer.appendChild(entryOptions);
     entryContainer.appendChild(entryDeleteTimeout);
     return entryContainer;
 };
 
 const createWorldEntry = (world, buttons) => {
-    return createEntry(world.id, world.name, world.thumbnailImageUrl, world.releaseStatus, buttons)
+    return createEntry(world, world.id, world.name, world.thumbnailImageUrl, world.releaseStatus, buttons)
 };
 
-const renderRow = (element, data, buttons) => {
-    for (let i = 0; i < data.length; i++) {
-        const wrld = data[i];
-        element.appendChild(createWorldEntry(wrld, buttons));
-    }
-};
-
-activeLoad.addEventListener("click", () => {
+// activeLoad.addEventListener("click", () => {
+const renderActive = () => {
     load();
     disableDiv(activeLoad);
-    getWorlds({active: true, offset: activePage}, (data) => {
-        stopLoading();
-        if (data.error === undefined) {
-            renderRow(activeWorlds, data, undefined);
-            const parent = activeLoad.parentElement;
-            activeWorlds.appendChild(parent);
-            activePage += 10;
-            blinkGreen();
-        } else {
-            blinkRed();
-            sendNotification("Error", data.error.message, getIconFor("error"));
+    // getWorlds({active: true, offset: activePage}, (data) => {
+    stopLoading();
+    const data = JSON.parse(fakeJson);
+    if (data.error === undefined) {
+        for (let i = 0; i < data.length; i++) {
+            const wrld = data[i];
+            let was = false;
+            for (let j = 0; j < prevActiveWorlds.length; j++) {
+                if (wrld.id === prevActiveWorlds[j]) {
+                    was = true;
+                }
+            }
+            if (was === false) {
+                activeWorlds.appendChild(createWorldEntry(wrld, []));
+                prevActiveWorlds.push(wrld.id);
+            }
         }
-        enableDiv(activeLoad);
-    })
-});
+        const parent = activeLoad.parentElement;
+        activeWorlds.appendChild(parent);
+        activePage += 10;
+        blinkGreen();
+    } else {
+        blinkRed();
+        sendNotification("Error", data.error.message, getIconFor("error"));
+    }
+    enableDiv(activeLoad);
+};
+
+    // })
+// });
 
 finishLoading();
